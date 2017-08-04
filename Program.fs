e9b39af5aa50818f48fb6246aae20e7f8cb7007a
Joshua Harms <joshua@nozzlegear.com>
2017-08-04 18:57:10 -0500

    Created a custom Env module rather than using one previously packaged with Suave

--- Program.fs
+++ Program.fs
@@ -1,162 +1,182 @@
 // Learn more about F# at http://fsharp.org
+namespace Program
 
 open System
 open System.Threading
 open System.Collections.Generic
 open System.Net
 open Newtonsoft.Json
+open Utils
 
-type TallyResponse = {
-    since: int64;
-    summary: Dictionary<string, int>;
-}
+module MainModule =
 
-type EmailTally = {
-    artist: string;
-    count: int;
-}
+    type TallyResponse = {
+        since: int64;
+        summary: Dictionary<string, int>;
+    }
 
-type SwuRecipient = {
-    name: string;
-    address: string;
-}
+    type EmailTally = {
+        artist: string;
+        count: int;
+    }
 
-type SwuSender = {
-    name: string;
-    address: string;
-    replyTo: string;
-}
+    type SwuRecipient = {
+        name: string;
+        address: string;
+    }
 
-type SwuTallyTemplateData = {
-    date: string;
-    tally: EmailTally seq
-}
+    type SwuSender = {
+        name: string;
+        address: string;
+        replyTo: string;
+    }
 
-type SwuMessage = {
-    template: string
-    recipient: SwuRecipient
-    cc: SwuRecipient list
-    sender: SwuSender
-    template_data: SwuTallyTemplateData
-}
+    type SwuTallyTemplateData = {
+        date: string;
+        tally: EmailTally seq
+    }
 
-type SwuResponse = {
-    test: bool
-}
+    type SwuMessage = {
+        template: string
+        recipient: SwuRecipient
+        cc: SwuRecipient list
+        sender: SwuSender
+        template_data: SwuTallyTemplateData
+    }
 
-let swuKey = Env.varRequired "ARTIST_TALLY_SWU_KEY"
-let swuTemplateId = Env.varRequired "ARTIST_TALLY_SWU_TEMPLATE_ID"
-let emailDomain = Env.varRequired "ARTIST_TALLY_EMAIL_DOMAIN"
-let apiDomain = Env.varDefault "ARTIST_TALLY_API_DOMAIN" "localhost:3000"
-let isLive = (Env.varDefault "ENV" "development") = "production"
+    type SwuResponse = {
+        test: bool
+    }
 
-let buildMessage<'bodyType> method url authHeader (body: 'bodyType option) = 
-    let uri = Uri url
-    let message = new Http.HttpRequestMessage ()
-    message.Method <- method
-    message.RequestUri <- uri
+    type IX = 
+        abstract member S: string
 
-    if body.IsSome then
-        let serialized = JsonConvert.SerializeObject body.Value
-        let content = new Http.StringContent (serialized, Text.Encoding.UTF8, "application/json")
+    type IY =
+        inherit IX
+        abstract member D: string
 
-        message.Content <- content
+    type Shwoop () = 
+        interface IY with
+            member x.S = ""
+            member x.D = ""
 
-    match authHeader with
-    | Some header -> message.Headers.Authorization <- header
-    | None -> ignore ()    
+        member x.SayHello name = printfn "Hello %s" name    
 
-    message
+    let t = Shwoop ()
 
-let makeRequest message = async {
-    use client = new Http.HttpClient ()    
-    let! response = client.SendAsync message |> Async.AwaitTask
+    let swuKey = Env.varRequired "ARTIST_TALLY_SWU_KEY"
+    let swuTemplateId = Env.varRequired "ARTIST_TALLY_SWU_TEMPLATE_ID"
+    let emailDomain = Env.varRequired "ARTIST_TALLY_EMAIL_DOMAIN"
+    let apiDomain = Env.varDefault "ARTIST_TALLY_API_DOMAIN" "localhost:3000"
+    let isLive = (Env.varDefault "ENV" "development") = "production"
 
-    response.EnsureSuccessStatusCode () |> ignore 
+    let buildMessage<'bodyType> method url authHeader (body: 'bodyType option) = 
+        let uri = Uri url
+        let message = new Http.HttpRequestMessage ()
+        message.Method <- method
+        message.RequestUri <- uri
 
-    let! body = response.Content.ReadAsStringAsync () |> Async.AwaitTask
+        if body.IsSome then
+            let serialized = JsonConvert.SerializeObject body.Value
+            let content = new Http.StringContent (serialized, Text.Encoding.UTF8, "application/json")
 
-    return body
-}
+            message.Content <- content
 
-let midnightYesterday () = 
-    let now = DateTime.Now
-    let midnight = DateTime (now.Year, now.Month, now.Day, 0, 0, 0)
-    let days = float -1;
+        match authHeader with
+        | Some header -> message.Headers.Authorization <- header
+        | None -> ignore ()    
 
-    midnight.AddDays days
+        message
 
-let toUnixTimestamp date = DateTimeOffset(date).ToUnixTimeMilliseconds()
+    let makeRequest message = async {
+        use client = new Http.HttpClient ()    
+        let! response = client.SendAsync message |> Async.AwaitTask
 
-let deserialize<'T> body = JsonConvert.DeserializeObject<'T> body 
+        response.EnsureSuccessStatusCode () |> ignore 
 
-let convertResponseToTally (summary: Dictionary<string, int>) = 
-    summary |> Seq.map(fun kvp -> { artist = kvp.Key; count = kvp.Value })
+        let! body = response.Content.ReadAsStringAsync () |> Async.AwaitTask
 
-let formatEmail name = sprintf "%s@%s" name emailDomain
+        return body
+    }
 
-let emailRecipient = 
-    if isLive then { name = "Mike"; address = formatEmail "mikef" }
-    else { name = "Joshua Harms"; address = formatEmail "josh" }
+    let midnightYesterday () = 
+        let now = DateTime.Now
+        let midnight = DateTime (now.Year, now.Month, now.Day, 0, 0, 0)
+        let days = float -1;
 
-let emailCcs =
-    if isLive then 
-        [
-            {
-                name = "Tim"
-                address = formatEmail "tim"
-            }
-            {
-                name = "Jeanette"
-                address = formatEmail "jeanette"
-            }
-            {
-                name = "Joshua Harms"
-                address = formatEmail "josh"
-            }
-        ]
-    else []    
+        midnight.AddDays days
 
-let sendEmailMessage (tally: seq<EmailTally>) = async {
-    let base64HeaderValue = sprintf "%s:" swuKey |> Text.Encoding.UTF8.GetBytes |> Convert.ToBase64String
-    let header = Http.Headers.AuthenticationHeaderValue ("Basic", base64HeaderValue)
-    let url = sprintf "https://api.sendwithus.com/api/v1/send"
-    let date = DateTime.Now.ToString ("MMM dd, yyyy")
-    let message = 
-        {
-            template = swuTemplateId
-            recipient = emailRecipient
-            cc = emailCcs
-            sender = 
+    let toUnixTimestamp date = DateTimeOffset(date).ToUnixTimeMilliseconds()
+
+    let deserialize<'T> body = JsonConvert.DeserializeObject<'T> body 
+
+    let convertResponseToTally (summary: Dictionary<string, int>) = 
+        summary |> Seq.map(fun kvp -> { artist = kvp.Key; count = kvp.Value })
+
+    let formatEmail name = sprintf "%s@%s" name emailDomain
+
+    let emailRecipient = 
+        if isLive then { name = "Mike"; address = formatEmail "mikef" }
+        else { name = "Joshua Harms"; address = formatEmail "josh" }
+
+    let emailCcs =
+        if isLive then 
+            [
                 {
-                    name = "KMSignalR Superintendent"
-                    address = formatEmail "superintendent"
-                    replyTo = formatEmail "superintendent" 
+                    name = "Tim"
+                    address = formatEmail "tim"
                 }
-            template_data = 
                 {
-                    date = date
-                    tally = tally
+                    name = "Jeanette"
+                    address = formatEmail "jeanette"
                 }
-        }
-    let! response = buildMessage Http.HttpMethod.Post url (Some header) (Some message) |> makeRequest
-    
-    return response |> deserialize<SwuResponse>
-}
+                {
+                    name = "Joshua Harms"
+                    address = formatEmail "josh"
+                }
+            ]
+        else []    
 
-[<EntryPoint>]
-let main argv =
-    let mayFifth2017 = 1493614800000L
-    let since = midnightYesterday () |> toUnixTimestamp
-    let protocol = if isLive then "https" else "http"
-    let url = sprintf "%s://%s/api/v1/orders/portraits/artist-tally?since=%i" protocol apiDomain since
-    let summaryResponse = buildMessage Http.HttpMethod.Get url None None |> makeRequest |> Async.RunSynchronously |> deserialize
+    let sendEmailMessage (tally: seq<EmailTally>) = async {
+        let base64HeaderValue = sprintf "%s:" swuKey |> Text.Encoding.UTF8.GetBytes |> Convert.ToBase64String
+        let header = Http.Headers.AuthenticationHeaderValue ("Basic", base64HeaderValue)
+        let url = sprintf "https://api.sendwithus.com/api/v1/send"
+        let date = DateTime.Now.ToString ("MMM dd, yyyy")
+        let message = 
+            {
+                template = swuTemplateId
+                recipient = emailRecipient
+                cc = emailCcs
+                sender = 
+                    {
+                        name = "KMSignalR Superintendent"
+                        address = formatEmail "superintendent"
+                        replyTo = formatEmail "superintendent" 
+                    }
+                template_data = 
+                    {
+                        date = date
+                        tally = tally
+                    }
+            }
+        let! response = buildMessage Http.HttpMethod.Post url (Some header) (Some message) |> makeRequest
+        
+        return response |> deserialize<SwuResponse>
+    }
 
-    if summaryResponse.summary.Count = 0 then 
-        printfn "Tally response contained an empty summary. Was the `since` parameter (%i) incorrect?" since
-    else 
-        for kvp in summaryResponse.summary do printfn "%s: %i portraits" kvp.Key kvp.Value
+    [<EntryPoint>]
+    let main argv =
+        let mayFifth2017 = 1493614800000L
+        let since = midnightYesterday () |> toUnixTimestamp
+        let protocol = if isLive then "https" else "http"
+        let url = sprintf "%s://%s/api/v1/orders/portraits/artist-tally?since=%i" protocol apiDomain since
+        let summaryResponse = buildMessage Http.HttpMethod.Get url None None |> makeRequest |> Async.RunSynchronously |> deserialize
 
-    let emailResponse = summaryResponse.summary |> convertResponseToTally |> sendEmailMessage |> Async.RunSynchronously
+        if summaryResponse.summary.Count = 0 then 
+            printfn "Tally response contained an empty summary. Was the `since` parameter (%i) incorrect?" since
+        else 
+            for kvp in summaryResponse.summary do printfn "%s: %i portraits" kvp.Key kvp.Value
 
-    0 // return an integer exit code
+        let emailResponse = summaryResponse.summary |> convertResponseToTally |> sendEmailMessage |> Async.RunSynchronously
+
+        0 // return an integer exit code
