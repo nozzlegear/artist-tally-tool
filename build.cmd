4df40398c271e698e59bba136013a7181d2415ab
Joshua Harms <joshua@nozzlegear.com>
2017-10-06 16:47:00 -0500

    Add paket bootstrapper

--- build.cmd
+++ build.cmd
@@ -1,7 +1,14 @@
 @echo off
 cls
 
-.paket\paket.exe restore --force
+echo "build.cmd running in directory %cd%"
+
+.paket/paket.bootstrapper.exe
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+.paket/paket.exe restore --force
 if errorlevel 1 (
   exit /b %errorlevel%
 )
