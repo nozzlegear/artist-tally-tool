f6f8b124fb76a89eaa5796d546019aa708085df2
Joshua Harms <joshua@nozzlegear.com>
2017-10-07 18:32:32 -0500

    Add c# timer to help debug

---
+++ cs-timer/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace CSTimer
+{
+    public class RandoClass
+    {
+        [Microsoft.Azure.WebJobs.FunctionNameAttribute("std-artist-tally-tool")]
+        public void Run(TimerInfo myTimer, TraceWriter log)
+        {
+            log.Info("My C# timer is running!!!!!")
+        }
+    }
+}
