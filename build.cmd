5301a6705c98dc3711bb56ae512b531352648c7a
Joshua Harms <joshua@nozzlegear.com>
2017-10-07 20:34:20 -0500

    df

--- build.cmd
+++ build.cmd
@@ -15,3 +15,6 @@ REM .paket\paket.exe restore --force
 REM if errorlevel 1 (
 REM   exit /b %errorlevel%
 REM )
+
+call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+IF !ERRORLEVEL! NEQ 0 goto error
